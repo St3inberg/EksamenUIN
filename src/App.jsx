cd1e50d0700d94c82acf88f9dfa16e6fa6052948
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-24 11:59:07 +0200

    har forsette på route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Router } from 'react-router-dom'
+import Layout from '/conponents/Layout'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,11 @@ function App() {
     <>
     <Router>
       <Layout>
-        <Route path={"/"}  element={<home/>}></Route>
+        <Route path={"/"}  element={<Home/>}></Route>
+        <Route path={"/event/:id"}  element={<EventPages/>}></Route>
+        
+
+
       </Layout>
     </Router>
     
