580b81b540ca225715aef63125d943ad4e07eb76
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-24 11:37:52 +0200

    begynt med router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Router } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <Layout>
+        <Route path={"/"}  element={<home/>}></Route>
+      </Layout>
+    </Router>
     
     </>
   )
