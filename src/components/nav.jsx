3f0891ee500dd11dfab3a94b28b82a97f0218a6e
danielsb03 <danielsb@hiof.no>
2025-04-30 15:55:00 +0200

    Gjort opp basics
    lagd et nytt komponent, fikset router, Linka opp komponenter og satt innhold i nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+
 export default function Nav () {
-    return (<ul>
-        <li>Home!</li>
-        <li>DashBoard</li>
-        <li>Eventpage</li>
-    </ul>)
+    const kategorier = [
+        {
+          id: 1,
+          name: "Teater",
+          slug: "teater"
+        },
+        {
+          id: 2,
+          name: "Broadway",
+          slug: "broadway"
+        },
+        {
+          id: 3,
+          name: "Kunst",
+          slug: "kunst"
+        }
+        
+    ]
+    return (<>
+    <ul>
+        <li><Link to="/dashboard">DashBoard</Link></li>
+    {kategorier.map((kategori) => (
+            
+            <li key={kategori.id}>
+              <Link to={`/kategori/${kategori.slug}`}>{kategori.name}!</Link>
+            </li>
+          ))}
+    </ul>
+    
+<h2><Link to="/">Logo</Link></h2>
+
+</>)
 }
