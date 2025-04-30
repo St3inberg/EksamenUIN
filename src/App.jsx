b758ba057b60d0602bc65ce78081e1be8f460724
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-30 16:41:27 +0200

    fikers import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 import './App.css'
-import { Router, Route } from 'react-router-dom'
-import Layout from '/conponents/Layout'
+import { Router, Route } from 'react-router-dom';
+import Layout from '/conponents/Layout';
+import Home from './components/Home';
+import EventPages from './components/EventPages'
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <Layout>
         <Route path={"/"}  element={<Home/>}></Route>
         <Route path={"/event/:id"}  element={<EventPages/>}></Route>
+        <Route path={"/category/:slug"}  element={<ArktikkeKort/>}></Route>
+        
 
 
       </Layout>
