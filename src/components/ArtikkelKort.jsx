6986a559efebdabafc667c7a44841922af883e58
Zeybo <sarahvg@hiof.no>
2025-04-30 16:27:47 +0200

    scss og start
    startet å legge til scss og ting på home og artikkelkort

--- src/components/ArtikkelKort.jsx
+++ src/components/ArtikkelKort.jsx
@@ -1,3 +1,20 @@
-export default function Artikkelkort () {
+export default function ArtikkelKort ({article, slug}) {
     return <h1>Dette er et artikkelkort</h1>
 }
+import { Link, useParams } from "react-router-dom";
+
+  
+
+  return (
+    <Link to={`profile/${person.productslug.current}`}>
+      
+      <article className="profilkort">
+      {person.imageUrl && (
+        <img src={person.imageUrl} alt={person.personname} />
+      )}
+        <h3 >{artikkel.artikkelNavn}</h3>
+       <p>{person.epost}</p>
+
+      </article>
+    </Link>
+  );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,30 @@
-export default function Home () {
-    return <h1>Velkommen til Hjemside!!</h1>
+export default function Home ({artikkel}) {
+    return <h1>Velkommen til BillettLyst!</h1>
 }
+
+import ArticleCard from "./ArtikkelKort";
+import Artikler from "./ArtikkelKort";
+// import Loggkort from './Loggkort';
+
+
+  return (
+
+    <>
+<section id='flex-container'>
+  {Artikler?.map((artikkel) => (
+        <ArtikkelKort artikkel={artikkel} key={person._id} />
+      ))}
+</section>
+<section >
+    <h2> Sommerens events :D </h2>
+          
+    {loggforing?.map((loggForhome) => (
+      <ArtikkelKort loggForhome={loggForhome} key={loggForhome._id}/>
+    ))}
+
+  </section>
+
+
+{/* Kan legge til fler h2 med andre overskrifter med andre artikkelKort under. */}
+  </>
+  )
