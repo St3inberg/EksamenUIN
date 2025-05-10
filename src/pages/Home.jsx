45d8653d334f8ac3cdacd8c68c61061f2028540e
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-05-10 12:26:44 +0200

    begyner på home dananosk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import HomeCard from "../components/cards/HomeCard";
+
 export default function Home() {
     return (
       <div className="container">
         <h1 className="section-title">Velkommen til Hjemside!!</h1>
+        <section>
+          {
+            // map.(){}
+          }
+          <HomeCard/>
+
+        </section>
+        
+        
         <p>Oppdag de feteste arrangementene innen musikk, teater og mer!</p>
       </div>
     );
