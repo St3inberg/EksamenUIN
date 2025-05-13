0f08a6e672d613d8518972261d77ff3a79a66ea1
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-05-13 15:55:07 +0200

    Update CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -2,6 +2,10 @@
 
 export default function CategoryPage(){
   
+  //https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=music&locale=* til Music 
+  // https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=theater&locale=* til tiater 
+  // https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=theater&locale=* til spott
+
 
 
   
@@ -10,9 +14,11 @@ export default function CategoryPage(){
 
 
   return (
-    <div>
+    <section>
+      <h2>musiker</h2>
+
       
-    </div>
+    </section>
   );
 };
 
