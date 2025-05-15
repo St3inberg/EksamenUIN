35878c9aeec862f2fbd56b6e199139753809e3f0
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-05-15 14:34:42 +0200

    Update CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,15 +1,30 @@
+import React, { useState } from 'react';
+import ticketmaster from './api/ticketmaster'
 
+export default function CategoryPage() {
 
-export default function CategoryPage(){
-  
   //https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=music&locale=* til Music 
   // https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=theater&locale=* til tiater 
   // https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=theater&locale=* til spott
 
 
 
-  
+
  
+
+  const [category, setCategory] = useState()
+
+    const  = async () => {
+     fetch("https://app.ticketmaster.com/discovery/v2/attractions?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&id=K8vZ917K7fV,%20K8vZ917_YJf,%20K8vZ917oWOV,%20K8vZ917bJC7&locale=*")
+     .then((response) => response.json())
+     .then((data) => setFindings(data._embedded.attractions))
+     .catch((error) => console.error("Skjedde noe dritt ved fetch findings", error));
+   
+   }
+
+
+
+
 
 
 
@@ -17,7 +32,7 @@ export default function CategoryPage(){
     <section>
       <h2>musiker</h2>
 
-      
+
     </section>
   );
 };
