832288d9a3105b20ee9a4d38df837473d88c1a08
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-05-16 10:39:52 +0200

    Update CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -7,27 +7,28 @@ export default function CategoryPage() {
   // https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=theater&locale=* til tiater 
   // https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=theater&locale=* til spott
 
+  const APIKEY = import.meta.env.VITE_TICKETMASTER_API_KEY;
 
-
+//setCategory(data._embedded.attractions))
 
  
 
   const [category, setCategory] = useState()
 
-
+    
 
     const fetchCategor = async () => {
      fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=plqRhuO50tPOhorgr6ODGoxDpMUYX6qC&keyword=music&locale=*")
      .then((response) => response.json())
-     .then((data) => setCategory(data._embedded.attractions))
+     .then((data) => setCategory(data._embedded))
      .catch((error) => console.error("Skjedde noe dritt ved fetch kategri", error));
    
    }
 
 
    useEffect(()=>{
-        setCategory()
-    console.log("katagori musik", category )
+        fetchCategor()
+    console.log("katagori musik", category)
    }, [])
 
 
